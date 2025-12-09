7026fe06ccbc964b02592054b66ecff0ce6bb0b9
mmadhan9247-png <mmadhan9247@gmail.com>
2025-12-09 18:16:34 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,13 @@
+let defaultBaseUrl = "http://localhost:5000";
+
+if (typeof window !== "undefined") {
+  const host = window.location.hostname;
+  if (host.includes("vercel.app")) {
+    defaultBaseUrl = "https://auth-backend-3oqm.onrender.com";
+  }
+}
+
 export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
+  process.env.REACT_APP_API_BASE_URL || defaultBaseUrl;
 
 export default API_BASE_URL;
