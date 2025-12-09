5cde1e5ab1ff66d2767e284e034f645077813234
mmadhan9247-png <mmadhan9247@gmail.com>
2025-12-09 18:16:59 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+const clientId =
+  process.env.REACT_APP_GOOGLE_CLIENT_ID ||
+  '680905646956-5i53ab61lg54mdlt2mcvn3q81vcqojmg.apps.googleusercontent.com';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
